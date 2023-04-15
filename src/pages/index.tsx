48b4648521fdf78bfbff599e3136d108ed3bcbea
Rashmi V Abbigeri <rashmiabbigeri@gmail.com>
2023-04-16 01:59:54 +0900

    commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,114 +1,92 @@
-import Head from "next/head";
 import {
-  Box,
-  Heading,
-  Container,
-  Text,
-  Button,
-  Stack,
-  Icon,
-  useColorModeValue,
-  createIcon,
-  HStack,
+  Link as ChakraLink,
   Image,
+  Text,
+  Code,
+  List,
+  ListIcon,
+  ListItem,
+  Button,
+  GridItem,
+  Input,
+  SimpleGrid,
+  VisuallyHidden,
+  Badge,
+  Box,
+  chakra,
+  Flex,
+  InputGroup,
+  InputLeftAddon,
+  Heading,
+  Center,
+  Icon,
+  Stack,
+  useColorModeValue,
+  HStack,
 } from "@chakra-ui/react";
+import { CheckCircleIcon, LinkIcon } from "@chakra-ui/icons";
+
+import { Hero } from "../components/Hero";
+import { Container } from "../components/Container";
+import { Main } from "../components/Main";
 import Header from "../components/Header";
+import { title } from "process";
+const Index = () => (
+<Container height="100vh"  bgColor={'blackAlpha.800'} >
+    <Header />
+    <Center mt="30vh" mb={'5vh'}>
+      <Heading textColor={"White"} fontSize="6vw">
+        Start your journey for free
+      </Heading>
+    </Center>
 
-export default function CallToActionWithAnnotation() {
-  return (
-    <>
-      <Header />
-      <Head>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Caveat:wght@700&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
-
-      <Container maxW={"3xl"} 
-      >
-        <Stack
-          as={Box}
-          textAlign={"center"}
-          spacing={{ base: 8, md: 14 }}
-          py={{ base: 20, md: 36 }}
+    <Flex>
+      <Center>
+      <Button
+          bg={"white"}
+          rounded={"2xl"}
+          color={"black"}
+          size={"lg"}
+          width={'120px'}
+          flex={"1 0 auto"}
+    
         >
-          <Heading
-            fontWeight={600}
-            fontSize={{ base: "2xl", sm: "4xl", md: "6xl" }}
-            lineHeight={"110%"}
-          >
-            Quick and Easy onboarding {" "}
-            <Text as={"span"} color={"black.400"}>
-             to Web3 Gaming
-            </Text>
-          </Heading>
-          <Text color={"gray.500"}>
-            Infrastructure to onboard one billion users to your game
-          </Text>
-          <Stack
-            direction={"column"}
-            spacing={3}
-            align={"center"}
-            alignSelf={"center"}
-            position={"relative"}
-          >
-            <Button
-              colorScheme={"black"}
-              color='black'
-              bg={"white"}
-              rounded={"full"}
-              px={6}
-              _hover={{
-                bg: "black.500",
-              }}
-            >
-              Get Started
-            </Button>
-            <Button variant={"link"} colorScheme={"blue"} size={"sm"}>
-              Learn more
-            </Button>
-            <Box>
-              <Icon
-                as={Arrow}
-                color={useColorModeValue("gray.800", "gray.300")}
-                w={71}
-                position={"absolute"}
-                right={-71}
-                top={"10px"}
-              />
-              <Text
-                fontSize={"lg"}
-                fontFamily={"Caveat"}
-                position={"absolute"}
-                right={"-125px"}
-                top={"-15px"}
-                transform={"rotate(10deg)"}
-              >
-                Starting at $15/mo
-              </Text>
-            </Box>
-          </Stack>
-        </Stack>
-      </Container>
-      <HStack height={'20vh'}>
+          Start
+        </Button>
+
+      </Center>
+      {/* <Stack spacing={4} direction={{ base: "column", md: "row" }} w={"full"}>
+        <InputGroup size={"lg"}>
+          <InputLeftAddon children="Email" width={'120px'} rounded={"2xl"} borderRightStyle='dashed' borderRightColor={'black'} borderRightWidth='3px' />
+
+          <Input
+            type={"text"}
+            size={"lg"}
+            placeholder={"superplay@game.com"}
+            color={"white"}
+            bg={"white"}
+            width={"2xl"}
+            rounded={"2xl"}
+            border={0}
+            _focus={{
+              bg: useColorModeValue("gray.200", "gray.800"),
+              outline: "none",
+            }}
+          />
+        </InputGroup>
+
+        
+      </Stack> */}
+    
+    </Flex>
+    <HStack width={'10vw'}>
         <Image src="safe.png" />
         <Image src="polygon-matic-logo.png" />
         <Image src="superfluid.svg" />
-      </HStack>
-    </>
-  );
-}
+    </HStack>
 
-const Arrow = createIcon({
-  displayName: "Arrow",
-  viewBox: "0 0 72 24",
-  path: (
-    <path
-      fillRule="evenodd"
-      clipRule="evenodd"
-      d="M0.600904 7.08166C0.764293 6.8879 1.01492 6.79004 1.26654 6.82177C2.83216 7.01918 5.20326 7.24581 7.54543 7.23964C9.92491 7.23338 12.1351 6.98464 13.4704 6.32142C13.84 6.13785 14.2885 6.28805 14.4722 6.65692C14.6559 7.02578 14.5052 7.47362 14.1356 7.6572C12.4625 8.48822 9.94063 8.72541 7.54852 8.7317C5.67514 8.73663 3.79547 8.5985 2.29921 8.44247C2.80955 9.59638 3.50943 10.6396 4.24665 11.7384C4.39435 11.9585 4.54354 12.1809 4.69301 12.4068C5.79543 14.0733 6.88128 15.8995 7.1179 18.2636C7.15893 18.6735 6.85928 19.0393 6.4486 19.0805C6.03792 19.1217 5.67174 18.8227 5.6307 18.4128C5.43271 16.4346 4.52957 14.868 3.4457 13.2296C3.3058 13.0181 3.16221 12.8046 3.01684 12.5885C2.05899 11.1646 1.02372 9.62564 0.457909 7.78069C0.383671 7.53862 0.437515 7.27541 0.600904 7.08166ZM5.52039 10.2248C5.77662 9.90161 6.24663 9.84687 6.57018 10.1025C16.4834 17.9344 29.9158 22.4064 42.0781 21.4773C54.1988 20.5514 65.0339 14.2748 69.9746 0.584299C70.1145 0.196597 70.5427 -0.0046455 70.931 0.134813C71.3193 0.274276 71.5206 0.70162 71.3807 1.08932C66.2105 15.4159 54.8056 22.0014 42.1913 22.965C29.6185 23.9254 15.8207 19.3142 5.64226 11.2727C5.31871 11.0171 5.26415 10.5479 5.52039 10.2248Z"
-      fill="currentColor"
-    />
-  ),
-});
+   
+  </Container>
+);
+
+export default Index;
