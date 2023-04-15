df8df48f20f61f85a46cfb898eee5e12f6cf6d62
flyinglimao <hi@limaois.me>
2023-04-16 03:54:47 +0900

    add simpler superfluid info getter

--- src/services/superFluid.ts
+++ src/services/superFluid.ts
@@ -1,6 +1,8 @@
 import { Framework } from "@superfluid-finance/sdk-core";
 import { web3AuthService } from "./web3Auth";
 import { formatEther } from "ethers/lib/utils";
+import { Contract } from "@ethersproject/contracts";
+import SuperPlayABI from "../../hardhat/artifacts/contracts/SuperPlay.sol/SuperPlay.json";
 
 type FlowInfo = {
   balance: string; // for display, formatted
@@ -10,7 +12,18 @@ type FlowInfo = {
 class SuperFluidService {
   private sf: Framework;
 
-  async getInfo(superToken: string, receiver: string): Promise<FlowInfo> {
+  async getInfo(gameId: number, superPlayAddress: string): Promise<FlowInfo> {
+    const superPlay = new Contract(
+      superPlayAddress,
+      SuperPlayABI.abi,
+      web3AuthService.getProvider().getSigner()
+    );
+    const { token, receiver } = await superPlay.games(gameId);
+
+    return this._getFlowInfo(token, receiver);
+  }
+
+  async _getFlowInfo(superToken: string, receiver: string): Promise<FlowInfo> {
     const token = await this.sf.loadSuperToken(superToken);
 
     const signer = web3AuthService.getProvider().getSigner();
