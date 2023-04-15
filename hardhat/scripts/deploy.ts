d26d7c1647be7617143fa6854ebaa5cfd810ee42
flyinglimao <hi@limaois.me>
2023-04-16 03:48:05 +0900

    add guard and module to deploy script

--- hardhat/scripts/deploy.ts
+++ hardhat/scripts/deploy.ts
@@ -8,10 +8,17 @@ async function main() {
   const superPlay = await SuperPlay.deploy(
     "0x94f26B4c8AD12B18c12f38E878618f7664bdcCE2"
   );
+  const Guard = await ethers.getContractFactory("SuperPlayGuard");
+  const guard = await Guard.deploy();
+  const Module = await ethers.getContractFactory("SuperPlayModule");
+  const module = await Module.deploy(superPlay.address);
 
   await superPlay.deployed();
+  await guard.deployed();
 
   console.log("SuperPlay deployed to:", superPlay.address);
+  console.log("SuperPlayGuard deployed to:", guard.address);
+  console.log("SuperPlayModule deployed to:", module.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
